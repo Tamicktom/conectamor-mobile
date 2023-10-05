f52e5c12376186a16f32134b46d1ecf63d904e3a
Tamicktom <rickpokemon38@gmail.com>
2023-10-04 21:07:21 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+//* Libraries imports
+import { View, Text } from "react-native";
+
+
+export default function Login() {
+  return (
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text>Login</Text>
+    </View>
+  );
+}
